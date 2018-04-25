98615224ccd1d6af5e5937e478cfa24e71f5548c
Cody Pauley <cpgameface@gmail.com>
2018-04-25 03:07:25 +0000

    basic backend

---
+++ app.py
@@ -0,0 +1,46 @@
+import tornado.ioloop
+import tornado.web
+import tornado.log
+import queries
+import os
+
+from jinja2 import \
+  Environment, PackageLoader, select_autoescape
+  
+ENV = Environment(
+  loader=PackageLoader('appfiles', 'templates'),
+  autoescape=select_autoescape(['html', 'xml'])
+)
+
+class TemplateHandler(tornado.web.RequestHandler):
+  def render_template (self, tpl, context=None):
+    if context is None:
+      context = {}
+      
+    template = ENV.get_template(tpl)
+    self.write(template.render(context))
+
+class MainHandler(TemplateHandler):
+  def get(self):
+    self.set_header(
+      'Cache-Control',
+      'no-store, no-cache, must-revalidate, max-age=0')
+    
+    self.render_template("index.html")
+    
+def make_app():
+  return tornado.web.Application([
+    (r"/", MainHandler),
+    (
+      r"/static/(.*)",
+      tornado.web.StaticFileHandler,
+      {'path': 'static'}
+    ),
+  ], autoreload=True)
+  
+if __name__ == "__main__":
+  tornado.log.enable_pretty_logging()
+  PORT = int(os.environ.get('PORT', '8080'))
+  app = make_app()
+  app.listen(PORT)
+  tornado.ioloop.IOLoop.current().start()
