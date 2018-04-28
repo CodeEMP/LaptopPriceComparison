206ef4bcf79d73f9db67e4c4eb4db58f63542c8b
Jim He <jimhe7@gmail.com>
2018-04-28 14:25:01 +0000

    apicall

--- apiCall.py
+++ apiCall.py
@@ -4,5 +4,8 @@ import requests
 r = requests.get('https://api.bestbuy.com/v1/products(bestSellingRank>0&(categoryPath.id=abcat0502000))?apiKey=APIKEY&sort=bestSellingRank.asc&show=bestSellingRank,manufacturer,salePrice,shortDescription,sku,name&pageSize=25&format=json')
 data = r.json()
 
-for a in range (0,26)
-    data['products'][a]
+for a in range (0,26):
+    data['products'][a]['name']
+    data['products'][a]['manufacturer']
+    data['products'][a]['sku']
+    data['products'][a]['salePrice']
