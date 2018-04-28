52a4f0685f8a8645e72d302a354654cde9b5b073
Jim He <jimhe7@gmail.com>
2018-04-28 16:59:46 +0000

    update

---
+++ createdb.sql
@@ -0,0 +1,13 @@
+CREATE TABLE apidata (
+  id SERIAL NOT NULL PRIMARY KEY,
+  sku varchar UNIQUE NOT NULL,
+  name varchar NOT NULL,
+  best_selling_rank integer,
+  manufacturer varchar,
+  sale_price decimal,    
+  short_description varchar,
+  image_url varchar,
+  regular_price decimal,
+  onsale boolean, 
+  as_of timestamp
+);
