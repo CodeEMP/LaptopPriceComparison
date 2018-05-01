4482fe5996890a0316d373299d74cc8119a7451a
Jim He <jimhe7@gmail.com>
2018-05-01 01:52:29 +0000

    more  sql

--- createdb.sql
+++ createdb.sql
@@ -17,3 +17,8 @@ CREATE TABLE apidata (
 --sql to get data from apidata, replace 6190769, probably have to use %s to replace python variable
 SELECT * FROM apidata
 WHERE (sku, as_of) IN (SELECT sku, MAX(as_of) FROM apidata GROUP BY sku) AND sku LIKE '6190769';
+
+--sql to get price history from apidata, replace 6190769, probably have to use %s to replace python variable
+SELECT sku, sale_price,as_of FROM apidata
+WHERE sku LIKE '6190769'
+ORDER BY as_of DESC;
