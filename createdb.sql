0bec6551dc5b0b02936a5d88748743dd7da3663f
Jim He <jimhe7@gmail.com>
2018-05-01 01:43:17 +0000

    timestamp works, add backend sql

--- createdb.sql
+++ createdb.sql
@@ -1,3 +1,5 @@
+--creates apidata table, where the api data is stored 
+
 CREATE TABLE apidata (
   id SERIAL NOT NULL PRIMARY KEY,
   sku varchar NOT NULL,
@@ -12,3 +14,6 @@ CREATE TABLE apidata (
   as_of timestamp
 );
 
+--sql to get data from apidata, replace 6190769, probably have to use %s to replace python variable
+SELECT * FROM apidata
+WHERE (sku, as_of) IN (SELECT sku, MAX(as_of) FROM apidata GROUP BY sku) AND sku LIKE '6190769';
