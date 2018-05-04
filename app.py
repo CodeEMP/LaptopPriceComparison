e112216bb921e561873af3be4104a7fb11cbe0fa
Cody Pauley <cpgameface@gmail.com>
2018-05-04 04:16:37 +0000

    list backend

--- app.py
+++ app.py
@@ -37,8 +37,6 @@ class MainHandler(TemplateHandler):
     self.redirect('/product/{}'.format(sku))
     
     
-    
-    
 class productHandler(TemplateHandler):
   def initialize(self):
       self.session = queries.Session(
@@ -48,8 +46,8 @@ class productHandler(TemplateHandler):
       'Cache-Control',
       'no-store, no-cache, must-revalidate, max-age=0')
     product = self.session.query(
-      'SELECT * FROM apidata WHERE (sku, as_of) IN (SELECT sku, MAX(as_of) FROM apidata GROUP BY sku) AND sku LIKE %(slug)s',
-      {'slug': slug}
+    'SELECT * FROM apidata WHERE (sku, as_of) IN (SELECT sku, MAX(as_of) FROM apidata GROUP BY sku) AND sku LIKE %(slug)s',
+    {'slug': slug}
     ).items()
     history = self.session.query(
       "SELECT sku, sale_price,as_of FROM apidata WHERE sku LIKE %(slug)s ORDER BY as_of DESC;",
@@ -59,13 +57,27 @@ class productHandler(TemplateHandler):
       'product' : product[0],
       'history' : history
     }
-    print(context['history'])
     self.render_template("product.html", context)
+
+
+class listHandler(TemplateHandler):
+  def initialize(self):
+      self.session = queries.Session(
+      'postgres://ukzppuglreogfo:017f117fb05419ba5c631061538bcf6f6220091f5b98fdbb4573882ab7fd65e2@ec2-54-83-204-6.compute-1.amazonaws.com:5432/dcnhjrhmqkb069')
+  def get(self):
+    self.set_header(
+      'Cache-Control',
+      'no-store, no-cache, must-revalidate, max-age=0')
+    product_list = self.session.query(
+      "SELECT sku, productname, sale_price FROM apidata WHERE (sku, as_of) IN (SELECT sku, MAX(as_of) FROM apidata GROUP BY sku) ORDER BY best_selling_rank ASC"
+      ).items()
+
     
 def make_app():
   return tornado.web.Application([
     (r"/", MainHandler),
     (r"/product/(.*)",productHandler),
+    (r"/list",listHandler),
     (
       r"/static/(.*)",
       tornado.web.StaticFileHandler,
